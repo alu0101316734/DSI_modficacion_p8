3494d073cab9c9abc67638aa667c99e53e113320
alu0101316734 <alu0101316734@ull.edu.es>
2024-03-13 10:45:05 +0000

    actualizamos con mas test

--- tests/modificacion1.spec.ts
+++ tests/modificacion1.spec.ts
@@ -4,13 +4,21 @@ import {Button,ButtonObserver,Observable,Observer,Event,} from "../src/modificac
 
 
 describe('Modificacion 1 - Obsever mod',() =>{
-   it('Comprobamos que funciona el get name',() =>{
+   it('Comprobamos que funciona el get id de Observador',() =>{
       const ButtonOberver= new ButtonObserver(0,"observador")
       expect(ButtonOberver.getId()).to.be.equal(0);
    })
    it('Comprobamos que funciona el get name',() =>{
-      const ButtonOberver= new ButtonObserver(0,"observable")
-      expect(ButtonOberver.getName()).to.be.equal("observable");
+      const ButtonOberver= new ButtonObserver(0,"observador")
+      expect(ButtonOberver.getName()).to.be.equal("observador");
    })
+   it('Comprobamos que funciona el get name',() =>{
+      const Button1= new Button<number>(0,"observable")
+      expect(Button1.getName()).to.be.equal("observable");
+   })  
+   it('Comprobamos que funciona el get name',() =>{
+      const Button1= new Button<number>(0,"observable")
+      expect(Button1.getId()).to.be.equal(0);
+   })  
    
 });
